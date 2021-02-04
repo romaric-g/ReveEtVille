9681183b0fc1db79bf24d8e3fea8a0aee071c436
romaric-g <romaric.gauzi1@gmail.com>
2021-02-04 13:59:12 +0100

    reset start

--- src/App.tsx
+++ src/App.tsx
@@ -802,7 +802,7 @@ const App = () => {
         console.log(action)
         console.log(currentStep)
         if (!currentStep) {
-            if (action.type === 'start') setCurrentStepID('49')
+            if (action.type === 'start') setCurrentStepID('1')
             return;
         }
         if (currentStep.type === 'choose' && action.type  === 'choose') {
